5d2b677ed4dea28473ea20c971764e3fb8c5d766
jack1377 <jack77.jacki77@gmail.com>
2025-10-19 09:00:42 +0200

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/temp_monitor/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.temp_monitor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // هیچ کدی اضافه نیاز نیست، v2 embedding همه چیز را هندل می‌کند
+}
