6dda4abee74cfdc032dadfc5fc833535458466e5
jack1377 <jack77.jacki77@gmail.com>
2025-10-19 09:16:08 +0200

    Delete android directory

--- android/app/src/main/kotlin/com/example/temp_monitor/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.temp_monitor
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-    // هیچ کدی اضافه نیاز نیست، v2 embedding همه چیز را هندل می‌کند
-}
